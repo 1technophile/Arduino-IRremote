5477d92e7657444c7bbdd73a8886a7e666e183b8
Florian <1technophile@users.noreply.github.com>
2017-03-22 19:31:48 +0100

    change interrupt pin to free receiver pin for RF

--- boarddefs.h
+++ boarddefs.h
@@ -134,8 +134,8 @@
 // Arduino Duemilanove, Diecimila, LilyPad, Mini, Fio, Nano, etc
 // ATmega48, ATmega88, ATmega168, ATmega328
 #else
-	//#define IR_USE_TIMER1   // tx = pin 9
-	#define IR_USE_TIMER2     // tx = pin 3
+	#define IR_USE_TIMER1   // tx = pin 9
+	//#define IR_USE_TIMER2     // tx = pin 3
 
 #endif
 
